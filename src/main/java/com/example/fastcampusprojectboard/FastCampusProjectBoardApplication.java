62dbd6e5a8fb3aafbfe57245c9ca1eeaf69cea24
Yeonwoo <yoanwoo0227@gmail.com>
2023-10-07 17:14:43 +0900

    #9 - 도메인 클래스 설계(게시글, 댓글)

--- src/main/java/com/example/fastcampusprojectboard/FastCampusProjectBoardApplication.java
+++ src/main/java/com/example/fastcampusprojectboard/FastCampusProjectBoardApplication.java
@@ -1,4 +1,4 @@
-package com.example.fastcampusprojectboard;
+package com.example.FastCampusProjectBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
