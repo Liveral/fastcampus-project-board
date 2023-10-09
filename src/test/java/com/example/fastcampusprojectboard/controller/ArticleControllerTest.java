e9372d75e61f06e5b7f84b461be86e0c2383eaec
Yeonwoo <yoanwoo0227@gmail.com>
2023-10-09 23:19:02 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러에 반영
    스프링 시큐리티가 자동 생성해준 '/login' 컨트롤러를 테스트

--- src/test/java/com/example/fastcampusprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.FastCampusProjectBoard.controller;
 
+import com.example.FastCampusProjectBoard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -14,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -25,28 +28,28 @@ class ArticleControllerTest {
 
     @DisplayName("[view]GET 게시글 리스트 페이지 - 정상호출")
     @Test
-    public void givenNoting_whenRequestingArticlesView_thenReturnsAtriclesView() throws Exception {
+    public void givenNoting_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
     }
-    @Disabled("구현 중")
+
     @DisplayName("[view]GET 게시글 상세 페이지 - 정상호출")
     @Test
-    public void givenNoting_whenRequestingArticleView_thenReturnsAtricleView() throws Exception {
+    public void givenNoting_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/detail"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("article"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"));
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("article"));
     }
     @Disabled("구현 중")
     @DisplayName("[view]GET 게시글 검색 페이지 - 정상호출")
     @Test
-    public void givenNoting_whenRequestingArticleSearchView_thenReturnsAtricleSearchView() throws Exception {
+    public void givenNoting_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("articles/search"))
